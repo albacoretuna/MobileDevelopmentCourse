77992596f43fa7f2b9e01ee8a9fc28bbc321583e
unknown <seyedhe@VW-BULEVARDI-07.pk.amk.root>
2014-11-13 09:59:55 +0200

    dir sctructure changed

---
+++ Basics and DOM/Ex 1/script2.js
@@ -0,0 +1,29 @@
+var temps = new Array ();
+
+for (var i = 0; i<9; i ++){
+	temps[i] = Math.ceil(Math.random()*30);
+	//console.log(temps[i]);
+	document.write(temps[i] + "<br>");
+
+}
+
+
+var minTemp; 
+minTemp = temps[0]; 
+//console.log(minTemp);
+var lowesTempNow;
+var j = 0; 
+while (j < temps.length) {
+
+if(temps[j] < minTemp) {
+	minTemp = temps[j];
+	console.log(minTemp); 
+}
+j += 1; 
+
+}
+
+
+
+
+document.write("Pienin Temp on:" + minTemp);

de468faaeb4c1a88981241d7d6d2b664cf824fb9
unknown <seyedhe@VW-BULEVARDI-07.pk.amk.root>
2014-11-13 10:53:35 +0200

    added Jquery examples

---
+++ jquery/ex 1/start-with-jqury.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+
+	// Hide parag including a word! 
+	//$("p:contains('first')").hide();
+	
+	// add a class 
+	//$("p:contains('first')").addClass("korosta");
+
+	$("p:last").append("Joohooo");
+
+}); 
---
+++ jquery/ex 2/sc.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+$("p:first").append("paragraph");
+$("p:last").remove();
+$("#fdiv").css("background-color","grey");
+$("#fdiv").append("<p>Added div</p>");
+
+$("#alink").text("Metropolia UAS");
+
+
+});
